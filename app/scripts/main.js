1945d8d011a4010430ea48d1eff1a69b34a1a431
Jose B. E. Vallecer <ZayZay@joses-mbp.attlocal.net>
2017-03-06 02:25:03 -0500

    cleaned up and finalized

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,6 +27,7 @@ console.log('\'Allo \'Allo!');
             $('#ucf').removeClass('animated fadeInLeft');
             $('#svad').removeClass('animated fadeInRight');
             $('.grad').removeClass('animated fadeInUp');
+            funnelChart();
           }
 
     }   
@@ -49,113 +50,43 @@ console.log('\'Allo \'Allo!');
 
    $('.carousel').carousel();
 
-  // d3
-
-  var bubbleChart = new d3.svg.BubbleChart({
-    supportResponsive: true,
-    //container: => use @default
-    size: 600,
-    //viewBoxSize: => use @default
-    innerRadius: 600 / 3.5,
-    //outerRadius: => use @default
-    radiusMin: 50,
-    //radiusMax: use @default
-    //intersectDelta: use @default
-    //intersectInc: use @default
-    //circleColor: use @default
-
-    data: {
-      items: [
-        {text: 'Java', count: '236'},
-        {text: '.Net', count: '382'},
-        {text: 'Php', count: '170'},
-        {text: 'Ruby', count: '123'},
-        {text: 'D', count: '12'},
-        {text: 'Python', count: '170'},
-        {text: 'C/C++', count: '382'},
-        {text: 'Pascal', count: '10'},
-        {text: 'Something', count: '170'},
-      ],
-      eval: function (item) {return item.count;},
-      classed: function (item) {return item.text.split(' ').join('');}
-    },
-    plugins: [
-      {
-        name: 'central-click',
-        options: {
-          text: '(See more detail)',
-          style: {
-            'font-size': '12px',
-            'font-style': 'italic',
-            'font-family': 'Source Sans Pro, sans-serif',
-            //"font-weight": "700",
-            'text-anchor': 'middle',
-            'fill': 'white'
-          },
-          attr: {dy: '65px'},
-          centralClick: function() {
-            alert('Here is more details!!');
-          }
-        }
-      },
-      {
-        name: 'lines',
-        options: {
-          format: [
-            {// Line #0
-              textField: 'count',
-              classed: {count: true},
-              style: {
-                'font-size': '28px',
-                'font-family': 'Source Sans Pro, sans-serif',
-                'text-anchor': 'middle',
-                fill: 'white'
-              },
-              attr: {
-                dy: '0px',
-                x: function (d) {return d.cx;},
-                y: function (d) {return d.cy;}
-              }
-            },
-            {// Line #1
-              textField: 'text',
-              classed: {text: true},
-              style: {
-                'font-size': '14px',
-                'font-family': 'Source Sans Pro, sans-serif',
-                'text-anchor': 'middle',
-                fill: 'white'
-              },
-              attr: {
-                dy: '20px',
-                x: function (d) {return d.cx;},
-                y: function (d) {return d.cy;}
-              }
-            }
-          ],
-          centralFormat: [
-            {// Line #0
-              style: {'font-size': '50px'},
-              attr: {}
-            },
-            {// Line #1
-              style: {'font-size': '30px'},
-              attr: {dy: '40px'}
-            }
-          ]
-        }
-      }]
-  });
-
 })(jQuery);
 
+// d3
 
-
-
-
-
-
-
-
-
+function funnelChart() {
+  var data = [
+['HTML', 70, '#71b6e1', '#FFFFFF'],
+['CSS', 50, '#e3b6fb', '#FFFFFF'],
+['Materialize', 80, '#ffc3e1', '#FFFFFF'],
+['jQuery', 40, '#ffe2a7', '#FFFFFF'],
+['JavaScript', 40, '#fffa9d', '#FFFFFF'],
+['PHP', 30, '#c6f8a6', '#FFFFFF'],
+['MySQL', 30, '#71b6e1', '#FFFFFF'],
+['SEO', 40, '#e3b6fb', '#FFFFFF'],
+['E-commerce', 30, '#ffc3e1', '#FFFFFF'],
+['Photoshop', 60, '#ffe2a7', '#FFFFFF'],
+['Illustrator', 60, '#fffa9d', 'FFFFFF'],
+['Flash', 40, '#c6f8a6', '#FFFFFF'],
+// Background ---^ ^--- Label
+];
+  var options = {
+    block: {
+    dynamicHeight: true,
+    highlight: true
+   },
+    chart: {
+    width: $(window).width() -200,
+    height: $(window).height() -200,
+    animate: 500
+   },
+    label: {
+    fontFamily: 'Helvetica',
+    fontSize: '20px'
+  }
+  };
+    d3.select('#funnel').selectAll('*').remove();
+    var chart = new D3Funnel('#funnel');
+    chart.draw(data, options);
+}
 
